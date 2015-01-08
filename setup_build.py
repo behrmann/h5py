7e651853509b6df0ac44b92b0c164ccf4f2b3353
Joerg Behrmann <behrmann@physik.fu-berlin.de>
2015-01-08 17:04:40 +0100

    This changeset implements a more robust configuration system for building h5py.
    Currently h5py cannot be installed with pip on a Debian system because
    the hdf5 library is not named libhdf5.so, due to Debian providing
    multiple version of the library (serial and mpi-enabled), and cannot be
    found at the place where the current configuration utility expect the
    file to be found, due to Debian being multiarch.
    
    This patch splits the autodection process into seperate steps for the
    library itself (path and name of the library seperately), the header
    files, and the version.
    
    For this the rewrite provides seperate functions
    autodetect_libdirs
    autodetect_libname
    autodetect_incluedirs
    autodetect_version
    
    All function should fall back to the current situation if they can not
    contribute any additional information.
    
    autodetect_libdirs tries to detect the library path using the ldconfig
    tool and falls back to te pkgconfig tool, which works sometimes, but
    e.g. under Debian is unable to provide information for the mpi-enabled
    library version (if both are installed)
    
    autodetect_includedirs falls back to the pkgconfig information as well,
    but has some hardwired information for where to find the header files on
    a Debian system.
    
    autodetect_libname now holds the logic for how the hdf5 library should
    be named and produces the appropriate regexp for autodetect_version and
    how the library should be named when including it in the build_process.
    
    All function have been amended to take as many additional parameters
    during install time as possible for this the following commandline
    switches have been introduced:
    
    --hdf5-librdir
    --hdf5-libname
    --hdf5-includedir
    
    all of them can also be accessed via the environment variables:
    
    HDF5_LIB
    HDF5_LIBNAME
    HDF5_INCLUDE
    
    additionaly an environment variable for the --mpi switch as been
    introduced.
    
    Conflicts:
    	setup_configure.py

--- setup_build.py
+++ setup_build.py
@@ -32,7 +32,7 @@ MODULES =  ['defs','_errors','_objects','_proxy', 'h5fd', 'h5z',
 
 EXTRA_SRC = {'h5z': [ localpath("lzf/lzf_filter.c"),
               localpath("lzf/lzf/lzf_c.c"),
-              localpath("lzf/lzf/lzf_d.c")]} 
+              localpath("lzf/lzf/lzf_d.c")]}
 
 
 if sys.platform.startswith('win'):
@@ -41,11 +41,11 @@ if sys.platform.startswith('win'):
         'include_dirs'  : [localpath('lzf'), localpath('windows')],
         'library_dirs'  : [],
         'define_macros' : [('H5_USE_16_API', None), ('_HDF5USEDLL_', None)] }
-        
+
 else:
     COMPILER_SETTINGS = {
        'libraries'      : ['hdf5', 'hdf5_hl'],
-       'include_dirs'   : [localpath('lzf'), '/opt/local/include', '/usr/local/include'],
+       'include_dirs'   : [localpath('lzf')],
        'library_dirs'   : ['/opt/local/lib', '/usr/local/lib'],
        'define_macros'  : [('H5_USE_16_API', None)] }
 
@@ -55,16 +55,16 @@ class h5py_build_ext(build_ext):
     """
         Custom distutils command which encapsulates api_gen pre-building,
         Cython building, and C compilation.
-        
+
         Also handles making the Extension modules, since we can't rely on
         NumPy being present in the main body of the setup script.
     """
-    
+
     @staticmethod
     def _make_extensions(config):
         """ Produce a list of Extension instances which can be passed to
         cythonize().
-        
+
         This is the point at which custom directories, MPI options, etc.
         enter the build process.
         """
@@ -86,27 +86,32 @@ class h5py_build_ext(build_ext):
         if config.mpi:
             import mpi4py
             settings['include_dirs'] += [mpi4py.get_include()]
-            
+
         # Ensure a custom location appears first, so we don't get a copy of
         # HDF5 from some default location in COMPILER_SETTINGS
-        if config.hdf5 is not None:
-            settings['include_dirs'].insert(0, op.join(config.hdf5, 'include'))
-            settings['library_dirs'].insert(0, op.join(config.hdf5, 'lib'))
+        if config.hdf5_libdir is not None:
+            settings['library_dirs'] += config.hdf5_libdir
+
+        if config.hdf5_includedir is not None:
+            settings['include_dirs'] += config.hdf5_includedir
+
+        if config.hdf5_libname is not None:
+            settings['libraries'] = config.hdf5_libname
 
         # TODO: should this only be done on UNIX?
         if os.name != 'nt':
             settings['runtime_library_dirs'] = settings['library_dirs']
-        
+
         def make_extension(module):
             sources = [localpath('h5py', module+'.pyx')] + EXTRA_SRC.get(module, [])
             return Extension('h5py.'+module, sources, **settings)
 
         return [make_extension(m) for m in MODULES]
-        
-        
+
+
     def run(self):
         """ Distutils calls this method to run the command """
-        
+
         from Cython.Build import cythonize
 
         # Provides all of our build options
@@ -116,12 +121,12 @@ class h5py_build_ext(build_ext):
         defs_file = localpath('h5py', 'defs.pyx')
         func_file = localpath('h5py', 'api_functions.txt')
         config_file = localpath('h5py', 'config.pxi')
-             
+
         # Rebuild low-level defs if missing or stale
         if not op.isfile(defs_file) or os.stat(func_file).st_mtime > os.stat(defs_file).st_mtime:
             print("Executing api_gen rebuild of defs")
             api_gen.run()
-            
+
         # Rewrite config.pxi file if needed
         if not op.isfile(config_file) or config.rebuild_required:
             with open(config_file, 'wb') as f:
@@ -135,13 +140,13 @@ DEF HDF5_VERSION = %(version)s
                       'version': tuple(int(x) for x in config.hdf5_version.split('.'))}
                 s = s.encode('utf-8')
                 f.write(s)
-        
+
         # Run Cython
         self.extensions = cythonize(self._make_extensions(config),
                             force=config.rebuild_required or self.force)
-        
+
         # Perform the build
         build_ext.run(self)
-        
+
         # Mark the configuration as built
         config.reset_rebuild()
